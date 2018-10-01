e9625b466093846bbfd0d4379c55e14f4c7a7922
n-t-roff <troff@arcor.de>
2018-10-01 17:22:06 +0200

    Bugfix: -p: Width of time text ignored

--- progress.c
+++ progress.c
@@ -7,12 +7,15 @@
 #include "summary.h"
 #include "main.h"
 
+static const time_t period = 10;
 short progress;
 
 inline static int time_elapsed(void) {
     static time_t t1;
     time_t t2 = time(NULL);
-    if (!t1 || t2 - t1 >= 10) /* every 10 seconds */
+    if (!t1)
+        t1 = t2;
+    else if (t2 - t1 >= period)
     {
         t1 = t2;
         return 1;
@@ -22,10 +25,8 @@ inline static int time_elapsed(void) {
 
 inline static int print_time(void) {
     static time_t t0;
-    if (!t0) {
-        t0 = time(NULL);
-        return 0;
-    }
+    if (!t0)
+        t0 = time(NULL) - period;
     time_t dt = time(NULL) - t0;
     int size = 0;
     time_t t = dt / (60 * 60); /* hours */
@@ -48,7 +49,7 @@ void show_progress(const char *const path, char *buf) {
     int cols_left = ti_get_cols();
     if (cols_left <= 0)
         return;
-    print_time();
+    cols_left -= print_time();
     cols_left -= printf("%'ld files %'jd bytes ",
                         total_file_count, (intmax_t)total_byte_count);
     if (cols_left <= 0)
